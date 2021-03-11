421bac1c7fcec67045de67f55711567527980448
Pankaj Mishra <pankaj.mishra2902@gmail.com>
2021-03-11 12:42:24 +0530

    location tracker

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
-import React, { Component } from 'react'
-import RouteComponent from './RouteComponent';
-import { Button } from 'react-bootstrap';
-// const limeOptions = { color: 'red' }
-//   const center = [51.505, -0.09]
-
-  // const polyline = [
-  //   [51.505, -0.09],
-  //   [51.51, -0.1],
-  //   [51.51, -0.12],
-  // ]
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import RouteComponent from "./RouteComponent";
+import { Button } from "react-bootstrap";
 export default class App extends Component {
-  
   render() {
     return (
       <div className="App">
-        { <Button style={{float:"left", marginTop:"10px", marginLeft:"20px"}} className="home-button">
-                    Home
-                  </Button>}
-        <RouteComponent/>
-        {/* <MapComponent/> */}
+        {
+          <Button
+            style={{ float: "left", marginTop: "10px", marginLeft: "20px" }}
+            className="home-button"
+          >
+            Home
+          </Button>
+        }
+        <RouteComponent />
       </div>
-    )
+    );
   }
 }
-
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react'
-import { MapContainer, TileLayer, Polyline} from 'react-leaflet'
-import "./MapComponent.css"
+import React, { Component } from "react";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import "./MapComponent.css";
 export default class MapComponent extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-           polyline : [],
-          limeOptions : { color: 'black' },
-          center : [20.5937, 78.9629]
-        }
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      polyline: [],
+      limeOptions: { color: "black" },
+      center: [20.5937, 78.9629],
+    };
+  }
 
-      static getDerivedStateFromProps(props, state) {
-        if (props.polyline !== state.polyline) {
-          return {
-            polyline: props.polyline,
-          };
-        }
-    
-        // Return null to indicate no change to state.
-        return null;
-      }
-    render() {
-        return (
-            <div id="mapid">
-      <MapContainer style={{height:"55vh"}} center={this.state.center} zoom={4} scrollWheelZoom={false}>
-     <TileLayer
-       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-     />
-     
-     <Polyline pathOptions={this.state.limeOptions} positions={this.state.polyline} />
-   </MapContainer>
-     </div>
-        )
+  static getDerivedStateFromProps(props, state) {
+    if (props.polyline !== state.polyline) {
+      return {
+        polyline: props.polyline,
+      };
     }
+    return null;
+  }
+  render() {
+    return (
+      <div id="mapid">
+        <MapContainer
+          style={{ height: "55vh" }}
+          center={this.state.center}
+          zoom={4}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <Polyline
+            pathOptions={this.state.limeOptions}
+            positions={this.state.polyline}
+          />
+        </MapContainer>
+      </div>
+    );
+  }
 }
